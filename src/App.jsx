0e4abc4b80ce5366cc64ac3817ccc8df94da7a2a
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2023-02-07 12:24:09 -0500

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      
+    </main>
   );
 }
 
---
+++ src/components/icons/Icon.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import IconRock from "./Rock";
+import IconPaper from "./Paper";
+import IconScissors from "./Scissors";
+import IconLizard from "./Lizard";
+import IconSpock from "./Spock";
+import IconClose from "./Close";
+import IconLogo from "./Logo";
+
+const Icon = ({ name }) => {
+	switch (name) {
+		case "Rock":
+			return <IconRock />;
+		case "Paper":
+			return <IconPaper />;
+		case "Scissors":
+			return <IconScissors />;
+		case "Lizard":
+			return <IconLizard />;
+		case "Spock":
+			return <IconSpock />;
+		case "Close":
+			return <IconClose />;
+		case 'Icon':
+			return <IconLogo/>;
+	}
+};
+
+Icon.propTypes = {
+	name: PropTypes.string.isRequired,
+};
+
+export default Icon;
