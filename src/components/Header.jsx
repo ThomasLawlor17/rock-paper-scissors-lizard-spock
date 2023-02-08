0695b896ded7c0c2a1fb25d2bc80cf3e83fcc02c
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2023-02-08 16:20:31 -0500

    Finish styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,41 +5,66 @@ const StyledHeader = styled.header`
 display: flex;
 align-items: center;
 justify-content: space-between;
-width: calc(84% - 24px);
-height: 76px;
-margin: 32px auto 0 auto;
-padding: 12px;
 border: solid 3px var(--header-outline);
 border-radius: 10px;
 position: relative;
+margin: 48px auto 0 auto;
+padding: 18px 24px 18px 30px;
+height: 114px;
+width: 646px;
+z-index: 4;
 
 .logo-icon {
-    scale: 0.47;
     position: relative;
-    left: -22px;
 }
 
 .score {
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 4px;
-    width: 85px;
-    height: 75px;
     background-color: #F9F9F9;
     border-radius: 5px;
+    height: 116px;
+    width: 152px;
+    gap: 10px;
 
     .score-title {
         color: var(--score-text);
-        font-size: 12px;
+        font-size: 18px;
         letter-spacing: 1px;
-        padding-top: 10px;
+        padding-top: 18px;
     }
     .score-number {
         color: var(--dark-text);
         font-weight: 700;
-        font-size: 40px;
-        line-height: 32px;
+        font-size: 64px;
+        line-height: 46px;
+    }
+}
+
+@media (max-width: 769px) {
+    width: calc(84% - 24px);
+    height: 76px;
+    margin: 32px auto 0 auto;
+    padding: 12px;
+
+    .logo-icon {
+        scale: 0.47;
+        left: -22px;
+    }
+    .score {
+        gap: 4px;
+        width: 85px;
+        height: 75px;
+
+        .score-title {
+            font-size: 12px;
+            padding-top: 10px;
+        }
+        .score-number {
+            font-size: 40px;
+            line-height: 32px;
+        }
     }
 }
 
--- src/components/MoveSelect.jsx
+++ src/components/MoveSelect.jsx
@@ -3,18 +3,16 @@ import Icon from "./icons/Icon"
 
 const StyledDiv = styled.div`
 width: 100%;
-max-width: 375px;
-margin: 4% auto auto;
-height: 400px;
 background-repeat: no-repeat;
-background-size: 58%;
-background-position: center 6rem;
+background-position: center;
 position: relative;
-
+width: 476px;
+height: 466px;
+margin: 45px auto 0 auto;
 
 .select {
-  height: 94px;
-  width: 94px;
+  width: 148px;
+  height: 148px;
   border-radius: 100%;
   display: flex;
   align-items: center;
@@ -26,47 +24,93 @@ position: relative;
 
   &::before {
     content: '';
+    width: 112px;
+    height: 112px;
     position: absolute;
-    width: 72px;
-    height: 72px;
     border-radius: 100%;
     background-color: #f9f9f9;
-    box-shadow: inset 0 4px lightgray;
+    box-shadow: inset 0 6px lightgray;
   }
-
-  .icon {
-    scale: 0.65;
+  svg {
+    z-index: 2;
   }
 }
 .select-Rock {
-  top: 16.6rem;
-  right: 4.9rem;
+  bottom: 0;
+  right: 66px;
 }
 .select-Paper {
-  top: 9rem;
-  right: 2.3rem;
+  top: 130px;
+  right: 0;
 }
 .select-Scissors {
-  top: 4rem;
+  top: 0;
   left: 0;
   right: 0;
 }
 .select-Lizard {
-  top: 16.6rem;
-  left: 4.8rem;
+  bottom: 0;
+  left: 66px;
 }
 .select-Spock {
-  top: 9rem;
-  left: 2.2rem;
+  top: 130px;
+  left: 0;
 }
+
+@media (max-width: 769px) {
+  max-width: 375px;
+  margin: 4% auto auto;
+  height: 400px;
+  background-size: 58%;
+  background-position: center 6rem;
+
+  .select {
+    height: 94px;
+    width: 94px;
+
+    &::before {
+      width: 72px;
+      height: 72px;
+      box-shadow: inset 0 4px lightgray;
+    }
+    .icon {
+      scale: 0.65;
+    }
+
+  }
+  .select-Rock {
+    top: 16.6rem;
+    right: 4.9rem;
+  }
+  .select-Paper {
+    top: 9rem;
+    right: 2.3rem;
+  }
+  .select-Scissors {
+    top: 4rem;
+    left: 0;
+    right: 0;
+  }
+  .select-Lizard {
+    top: 16.6rem;
+    left: 4.8rem;
+  }
+  .select-Spock {
+    top: 9rem;
+    left: 2.2rem;
+  }
+}
+
 
 `
 
-const MoveSelect = ({handleUserMove, moves}) => {
+const MoveSelect = ({handleUserMove, moves, width}) => {
+
+
   return (
     <StyledDiv className="moves-container">
         {moves && moves.map(({move, name, beats}) => (
-            <div key={move} className={"select select-" + name} onClick={() => handleUserMove(move, name, beats)} id={move} style={{'background': `var(--${name}-gradient)`, 'boxShadow': `inset 0 -4px var(--${name}-shadow)`}}>
+            <div key={move} className={"select select-" + name} onClick={() => handleUserMove(move, name, beats)} id={move} style={{'background': `var(--${name}-gradient)`, 'boxShadow': `inset 0 ${width > 770 ? '-7px' : '-4px'} var(--${name}-shadow)`}}>
                 <Icon name={name} />
             </div>
         ))}
